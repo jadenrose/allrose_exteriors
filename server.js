f7c9c0cb0773f7cc5a2f2e8bc8ea6ae2c5929073
Jaden Rose <jadenrrose@gmail.com>
2022-03-01 14:50:43 -0700

    force https w heroku headers

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const enforce = require('express-sslify')
 const cors = require('cors')
 const path = require('path')
 const serveStatic = require('serve-static')
@@ -7,7 +6,22 @@ const history = require('connect-history-api-fallback')
 
 const app = express()
 
-app.use(enforce.HTTPS())
+app.all('*', (req, res, next) => {
+	//Heroku stores the origin protocol in a header variable. The app itself is isolated within the dyno and all request objects have an HTTP protocol.
+	if (
+		req.get('X-Forwarded-Proto') === 'https' ||
+		req.hostname === 'localhost'
+	) {
+		//Serve Angular App by passing control to the next middleware
+		next()
+	} else if (
+		req.get('X-Forwarded-Proto') !== 'https' &&
+		req.get('X-Forwarded-Port') !== '443'
+	) {
+		//Redirect if not HTTP with original request URL
+		res.redirect('https://' + req.hostname + req.url)
+	}
+})
 
 const db = require(path.join(__dirname, 'db'))
 
